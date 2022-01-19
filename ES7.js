44294a5e478ea07e8beecf707f4ea3188c4fb2ab
cldrojas <cld.rojasz@gmail.com>
2022-01-19 01:05:30 -0300

    add ecmascript 7 features

---
+++ ES7.js
@@ -0,0 +1,20 @@
+/* 
+
+EcmaScript 7 - released on: 2016
+
+*/
+
+// includes
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+if (numbers.includes(4)) {
+  // string or array
+  console.log("number 4 is in the array");
+}
+
+// potentiation
+let base = 2;
+let exponent = 3;
+
+let result = base ** exponent;
+console.log(result);
